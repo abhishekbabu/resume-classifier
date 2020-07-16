5e92352e4c06cb45f96a9007a03e6c381fe5a43f
Abhishek Babu <abhideep0408@gmail.com>
2020-07-15 19:11:02 -0700

    Revert "Created Node/Express scaffolding with simple landing page"
    This reverts commit 6b36f9bc470d398b0cc1d1989c95edba698fa8fb.

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Resume Screener, a project for CodeDay Labs' });
-});
-
-module.exports = router;
