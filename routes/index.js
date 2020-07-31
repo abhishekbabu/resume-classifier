6caab1d567bbbdb9d14645ccd234cbd6293fbae8
Abhishek Babu <abhideep0408@gmail.com>
2020-07-30 22:45:16 -0700

    Updated page to show result on right half of page after sending user input to Python script and receiving the output

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Resume Screener' });
+  res.render('index', { result: '', resume: '' });
 });
+
+router.use(express.urlencoded());
+router.use(express.json());
+
+/* POST to get screening result */
+const {spawn} = require('child_process');
+router.post('/', function(req, res) {
+  const scriptPath = 'test.py'
+  const process = spawn('python', [scriptPath, req.body.resume])
+  var category;
+  process.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    category = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  process.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.render('index', { result: category, resume: req.body.resume })
+  });
+})
 
 module.exports = router;
