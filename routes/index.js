6feebbad43bb21e37cff7c8babed14af0e05e689
Abhishek Babu <abhideep0408@gmail.com>
2020-08-01 19:37:41 -0700

    Displaying top 5 results now with updated model; also added ipynb notebook

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { result: '', resume: '' });
+  res.render('index', { title: '',
+                        resume: '',
+                        result1: '',
+                        result2: '',
+                        result3: '',
+                        result4: '',
+                        result5: '' });
 });
 
 router.use(express.urlencoded());
@@ -12,12 +18,14 @@ router.use(express.json());
 /* POST to get screening result */
 const {spawn} = require('child_process');
 router.post('/', function(req, res) {
-  const scriptPath = 'test.py'
+  const scriptPath = 'resume_screener.py'
   const process = spawn('python', [scriptPath, req.body.resume])
   var category;
   process.stdout.on('data', function (data) {
     console.log('Pipe data from python script ...');
     category = data.toString();
+    category = category.split(/[\r\n]+/)
+    console.log(category)
   });
 
   process.stderr.on('data', function (data) {
@@ -27,7 +35,13 @@ router.post('/', function(req, res) {
   process.on('close', (code) => {
     console.log(`child process close all stdio with code ${code}`);
     // send data to browser
-    res.render('index', { result: category, resume: req.body.resume })
+    res.render('index', { title: 'Top 5 Results',
+                          resume: req.body.resume,
+                          result1: category[0],
+                          result2: category[1],
+                          result3: category[2],
+                          result4: category[3],
+                          result5: category[4] })
   });
 })
 
